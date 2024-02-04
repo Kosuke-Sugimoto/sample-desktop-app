fde722d7848e31a6ed05a751c02c776f1a42bd7c
Kosuke-Sugimoto <ks.1285.main@gmail.com>
2024-02-05 02:16:24 +0900

    add function to load Image

--- client/common/compose-ui/src/commonMain/kotlin/ui/components/MyImage.kt
+++ client/common/compose-ui/src/commonMain/kotlin/ui/components/MyImage.kt
@@ -1,11 +1,16 @@
 package me.sugimoto.sampleapp.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import me.sugimoto.sampleapp.ui.theme.JetsnackColors
+import me.sugimoto.sampleapp.ui.theme.JetsnackTheme
 
 @Composable
 fun MyImage(
@@ -17,7 +22,7 @@ fun MyImage(
     Surface(
         elevation = elevation
     ) {
-        AsyncImage(imageUri, contentDescription, Modifier.fillMaxSize())
+        AsyncImage(imageUri, contentDescription, modifier.fillMaxSize())
     }
 }
 
--- client/common/compose-ui/src/desktopMain/kotlin/ui/components/MyImageDesktop.kt
+++ client/common/compose-ui/src/desktopMain/kotlin/ui/components/MyImageDesktop.kt
@@ -3,14 +3,20 @@ package me.sugimoto.sampleapp.ui.components
 import androidx.compose.animation.*
 import androidx.compose.animation.core.TweenSpec
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.toComposeImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import me.sugimoto.sampleapp.ui.theme.JetsnackTheme
 import java.net.URL
 import javax.imageio.ImageIO
 
@@ -39,7 +45,14 @@ actual fun AsyncImage(
                 contentScale = ContentScale.Crop
             )
         } else {
-            Box(modifier = modifier)
+            Text(
+                "No Content",
+                color = JetsnackTheme.colors.textPrimary,
+                textAlign = TextAlign.Center,
+                modifier = modifier
+                    .fillMaxSize()
+                    .background(color = JetsnackTheme.colors.uiBackground)
+            )
         }
     }
     
